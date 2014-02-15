a295704f6d35efa7b1bc5f43b22965528f1bf4ce
B1nTree <dylanpierce914@gmail.com>
2014-02-15 01:21:28 -0500

    Did a lot

--- app/controllers/RoomController.php
+++ app/controllers/RoomController.php
@@ -5,12 +5,18 @@ class RoomController extends BaseController {
 	public function index()
 	{
 		$rooms = Room::all();
-		return View::make('Rooms.index')->with($rooms);
+		return View::make('Rooms.index')->with('rooms', $rooms);
 	}
 
 	public function add()
 	{
+		return View::make('Rooms.add');
+	}
 
+	public function handleAdd()
+	{
+		$room = Room::new();
+		return View::make('Rooms.index')->with('message', 'Room ' . $room->name . ' has been created');
 	}
 
 	public function remove()
